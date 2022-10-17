fc1b6dc973c7013f2a3599cbda54ed7784e2e02c
Itahand Naizir <itahand.naizir@gmail.com>
2022-10-17 16:03:33 -0500

    Setting up the project.
    Installing dependencies and changing components from React to React-Native.

---
+++ screens/Home.js
@@ -0,0 +1,15 @@
+import { Text } from 'react-native'
+
+const Home = () => {
+  return (
+    <Text>
+      Home
+      fpr
+      times
+      efew
+      wef
+    </Text>
+  )
+}
+
+export default Home
