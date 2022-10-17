7e253c7d65da6266f9a44f2ede347587ff05ec97
Itahand Naizir <itahand.naizir@gmail.com>
2022-10-17 16:16:17 -0500

    Started creating components.

---
+++ components/FocusedStatusBar.js
@@ -0,0 +1,14 @@
+import { Text, View } from 'react-native'
+import React, { Component } from 'react'
+
+export class FocusedStatusBar extends Component {
+  render() {
+    return (
+      <View>
+        <Text>FocusedStatusBar</Text>
+      </View>
+    )
+  }
+}
+
+export default FocusedStatusBar
